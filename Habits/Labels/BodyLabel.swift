0f112088f81a6484c9711a0dcd6c660d51d4002c
Alex <alexthompsondevelopment@gmail.com>
2021-04-26 12:24:52 +1000

    build out tableView cell

---
+++ Habits/Labels/BodyLabel.swift
@@ -0,0 +1,35 @@
+//
+//  BodyLabel.swift
+//  Habits
+//
+//  Created by Alexander Thompson on 26/4/21.
+//
+
+import UIKit
+
+class BodyLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    convenience init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        self.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
---
+++ Habits/Screens/AddHabitVC.swift
@@ -0,0 +1,23 @@
+//
+//  AddHabitVC.swift
+//  Habits
+//
+//  Created by Alexander Thompson on 26/4/21.
+//
+
+import UIKit
+
+class AddHabitVC: UIViewController {
+
+    //create a label file to implement here. 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+    
+
+  
+
+}
