2fcffc78bd226b897b507a97e90a6e6e61e1afae
Alex <alexthompsondevelopment@gmail.com>
2021-04-27 11:30:47 +1000

    set up buttons for color change

---
+++ Habits/Buttons/ColorButton.swift
@@ -0,0 +1,33 @@
+//
+//  ColorButton.swift
+//  Habits
+//
+//  Created by Alexander Thompson on 27/4/21.
+//
+
+import UIKit
+
+class ColorButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(backgroundColor: UIColor) {
+        self.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+    }
+    
+    private func configure() {
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.white.cgColor
+        self.layer.cornerRadius = self.frame.height / 2
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+}
