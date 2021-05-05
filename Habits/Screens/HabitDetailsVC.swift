16fc1d56289eb051da1a78354230686d05c5ef5b
Alex Thompson <AlexThompsonDevelopment@gmail.com>
2021-05-05 15:59:18 +1000

    build out calendar functionality

--- Habits/Screens/HabitDetailsVC.swift
+++ Habits/Screens/HabitDetailsVC.swift
@@ -16,6 +16,13 @@ class HabitDetailsVC: UIViewController {
     var habitData = HabitData()
     var addHabitVC = AddHabitVC()
     let calendarView = CalendarView()
+    let dateArray: [Date] = []
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let date = Date()
+        self.calendarView.setDisplayDate(date)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +30,7 @@ class HabitDetailsVC: UIViewController {
         configureBarButtons()
         configureCalendarView()
         self.tabBarController?.tabBar.isHidden = true
-        calendarView.delegate = self
+       
     }
     
     private func configureBarButtons() {
@@ -36,9 +43,33 @@ class HabitDetailsVC: UIViewController {
     }
  
     func configureCalendarView() {
+        view.addSubview(calendarView)
+        calendarView.delegate = self
+        calendarView.dataSource = self
         calendarView.translatesAutoresizingMaskIntoConstraints = false
-        //calendarView.style = .Default
-        calendarView.frame = view.bounds
+        calendarView.direction = .horizontal
+        calendarView.style.locale = Locale.current
+        calendarView.style.weekdaysBackgroundColor = .blue
+        
+        let myStyle = CalendarView.Style()
+        myStyle.cellBorderColor = UIColor.black
+        myStyle.cellBorderWidth = 2.0
+        self.calendarView.style = myStyle
+        myStyle.cellShape = CalendarView.Style.CellShapeOptions.round
+        
+        myStyle.cellSelectedBorderColor = HabitArray.Array[cellTag].buttonColor!
+        myStyle.cellTextColorDefault = .label
+        myStyle.cellSelectedTextColor = .label
+        //use mystyle to set all the relevent colors and selections
+        
+        NSLayoutConstraint.activate([
+            calendarView.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            calendarView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            calendarView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            calendarView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        
+        ])
+        
     }
     
     private func configureViewController() {
@@ -59,36 +90,48 @@ class HabitDetailsVC: UIViewController {
 }
 
 extension HabitDetailsVC: CalendarViewDelegate, CalendarViewDataSource {
+    
+    
     func calendar(_ calendar: CalendarView, didScrollToMonth date: Date) {
-        <#code#>
+    
     }
     
     func calendar(_ calendar: CalendarView, didSelectDate date: Date, withEvents events: [CalendarEvent]) {
-        <#code#>
+        //build in equation to determine current and longest streak based on days selected.
+        
+        
     }
     
     func calendar(_ calendar: CalendarView, canSelectDate date: Date) -> Bool {
-        <#code#>
+        return true
     }
     
     func calendar(_ calendar: CalendarView, didDeselectDate date: Date) {
-        <#code#>
+       
     }
     
     func calendar(_ calendar: CalendarView, didLongPressDate date: Date, withEvents events: [CalendarEvent]?) {
-        <#code#>
+        
     }
     
     func startDate() -> Date {
-        <#code#>
+        var dateComponents = DateComponents()
+        dateComponents.month = -12
+        let today = Date()
+        let twelveMonthsAgo = self.calendarView.calendar.date(byAdding: dateComponents, to: today)
+        return twelveMonthsAgo!
     }
     
     func endDate() -> Date {
-        <#code#>
+        var dateComponents = DateComponents()
+        dateComponents.month = 12
+        let today = Date()
+        let twelveMonthsFromNow = self.calendarView.calendar.date(byAdding: dateComponents, to: today)
+        return twelveMonthsFromNow!
     }
     
     func headerString(_ date: Date) -> String? {
-        <#code#>
+        return nil
     }
     
     
