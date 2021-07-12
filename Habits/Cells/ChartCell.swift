8e3df7848ced59fad69a98352a3e6b304a46b3a7
Alex Thompson <AlexThompsonDevelopment@gmail.com>
2021-07-12 22:35:26 +1000

    create proper data sorting & counting for chart view

--- Habits/Cells/ChartCell.swift
+++ Habits/Cells/ChartCell.swift
@@ -10,35 +10,23 @@ import UIKit
 class ChartCell: UICollectionViewCell {
  
     static let reuseID = "ChartCell"
-    
-    var cellView = UIView()
+    let habitView = HabitCountView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        configure()
+      configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
     private func configure() {
-        cellView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(cellView)
-       
-        NSLayoutConstraint.activate([
-
-            cellView.topAnchor.constraint(equalTo: self.topAnchor),
-            cellView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            cellView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            cellView.bottomAnchor.constraint(equalTo: self.bottomAnchor),
-            
-    ])}
+        
+        contentView.addSubview(habitView)
+        habitView.frame = contentView.bounds
+        habitView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+    }
+    
+    
 }
-    
-    //complete chart cell. create a view to insert charts.
-    
-    //build out year array as planned. follow dribble for look.
-    
-
