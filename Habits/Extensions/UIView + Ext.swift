f8a0fd877eaaf878315175d5bb13c1f67071e2ce
Alexander Thompson <alexThompsonDevelopment@gmail.com>
2021-11-29 20:57:14 +1100

    Build in loading screen and update icon to new style

--- Habits/Extensions/UIView + Ext.swift
+++ Habits/Extensions/UIView + Ext.swift
@@ -39,6 +39,13 @@ extension UIView {
         }
     }
     
-    
+    func rotate() {
+        let rotation: CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotation.toValue = NSNumber(value: Double.pi * 2)
+        rotation.duration = 2
+        rotation.isCumulative = true
+        rotation.repeatCount = Float.greatestFiniteMagnitude
+        self.layer.add(rotation, forKey: "rotationAnimation")
+    }
     
 }
---
+++ Habits/Screens/IntroScreenViewController.swift
@@ -0,0 +1,55 @@
+//
+//  IntroScreenViewController.swift
+//  Habits
+//
+//  Created by Alexander Thompson on 29/11/21.
+//
+
+import UIKit
+
+class IntroScreenViewController: UIViewController {
+
+    let iconCircle = UIImageView(image: UIImage(named: "iconCircle")?.addTintGradient(colors: GradientArray.array[5]))
+    let icontick = UIImageView(image: UIImage(named: "iconTick")?.addTintGradient(colors: GradientArray.array[5]))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       configure()
+        iconCircle.rotate()
+        presentHabitVC()
+    }
+    
+    private func configure() {
+        view.backgroundColor = .systemBackground
+        
+        iconCircle.translatesAutoresizingMaskIntoConstraints = false
+        icontick.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubviews(iconCircle, icontick)
+        
+        NSLayoutConstraint.activate([
+            iconCircle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            iconCircle.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            iconCircle.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25),
+            iconCircle.widthAnchor.constraint(equalTo: iconCircle.heightAnchor),
+        
+            icontick.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            icontick.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            icontick.heightAnchor.constraint(equalTo: iconCircle.heightAnchor, multiplier: 0.45),
+            icontick.widthAnchor.constraint(equalTo: icontick.heightAnchor),
+        ])
+    }
+
+    func presentHabitVC() {
+        let destVC = UINavigationController(rootViewController: HabitVC())
+        destVC.modalPresentationStyle = .fullScreen
+        
+        let seconds = 2.0
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            self.present(destVC, animated: true)
+        }
+        
+    }
+  
+    }
+
