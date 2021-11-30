e94c49779e574586bc6fbfa147a959fde0475205
Alexander Thompson <alexThompsonDevelopment@gmail.com>
2021-11-30 12:41:51 +1100

    Fix new habit spacing issue

--- Habits/Extensions/Animations.swift
+++ Habits/Extensions/Animations.swift
@@ -36,4 +36,17 @@ extension UIImageView {
     }
 }
 
+extension UIView {
+    
+    func rotate() {
+        let rotation: CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.z")
+        rotation.toValue = NSNumber(value: Double.pi * 2)
+        rotation.duration = 2
+        rotation.isCumulative = true
+        rotation.repeatCount = Float.greatestFiniteMagnitude
+        self.layer.add(rotation, forKey: "rotationAnimation")
+    }
+    
+    }
+    
 
