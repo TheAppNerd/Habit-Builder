eb8ea96da3b57ec4157e7bdbcd089a70c4246a83
Alex Thompson <alexThompsonDevelopment@gmail.com>
2022-04-08 12:09:01 +1000

    fix merge error

---
+++ Habits/Model/EmailFeedback.swift
@@ -0,0 +1,51 @@
+//
+//  EmailFeedback.swift
+//  Habits
+//
+//  Created by Alexander Thompson on 7/12/21.
+//
+
+import UIKit
+
+struct EmailFeedback {
+    
+    func newEmail() {
+        //move to constants
+        let address = "Alex@appNerd.com.au"
+        let subject = "Habit Builder Feedback"
+        
+        let appVersion = UIApplication.appVersion
+        let systemVersion = UIDevice.current.systemVersion
+        let locale = Locale.current
+        let modelNumber = UIDevice().modelIdentifier()
+        
+        
+        let body = """
+
+
+
+Aussie News Version: \(appVersion ?? "Unable to Locate")
+iOS Version: \(systemVersion)
+Device: \(modelNumber)
+Location: \(locale)
+
+"""
+        
+        var components = URLComponents()
+        components.scheme = "mailto"
+        components.path = address
+        components.queryItems = [
+            URLQueryItem(name: "subject", value: subject),
+            URLQueryItem(name: "body", value: body)
+        ]
+        
+        guard let url = components.url else {
+            print("Failed to create mailto URL")
+            return
+        }
+        UIApplication.shared.open(url)
+    }
+}
+
+
+
